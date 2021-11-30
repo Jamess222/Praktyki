323ccfc988998a11560a053585e63c37c027f8d4
Jamess222 <95212211+Jamess222@users.noreply.github.com>
2021-11-30 14:15:40 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void changeText_Click(object sender, EventArgs e)
+        {
+            if(enableCheckbox.Checked == true)
+            {
+                if(labelToChange.Text == "Z prawej")
+                {
+                    labelToChange.Text = "Z lewej";
+                    labelToChange.TextAlign = ContentAlignment.MiddleLeft;
+                }
+                else
+                {
+                    labelToChange.Text = "Z prawej";
+                    labelToChange.TextAlign = ContentAlignment.MiddleRight;
+                }
+
+            }
+            else
+            {
+                labelToChange.Text = "Mozliwosc zmiany textu zostala wylaczona";
+                labelToChange.TextAlign = ContentAlignment.MiddleCenter;
+            }
+        }
+    }
+}
